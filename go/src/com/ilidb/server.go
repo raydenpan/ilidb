277ac79e4f6ffc03ae146156fe58f334f299f223
John <lindskog.john@gmail.com>
2017-07-14 16:35:31 +0200

    Static file server listening on port 9080

--- go/src/com/ilidb/server.go
+++ go/src/com/ilidb/server.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+	//Print server starting message
+	fmt.Printf("Starting Ilidb.com server...\n")
+
+	// Create file server for static html files
+	fs := http.FileServer(http.Dir("../../../../html"))
+
+	//Main page, index.html
+	http.Handle("/", fs)
+
+	//Listen on port 9080
+	http.ListenAndServe(":9080", nil)
 }
